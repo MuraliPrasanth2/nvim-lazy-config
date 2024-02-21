f53a25a2fdaff082fa690f48daace28e480e7e32
Murali Prasanth <mprasanth18@gmail.com>
2024-02-21 14:26:53 +0530

    added neotree and modularized the plgins scripts

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,9 @@
+return { "catppuccin/nvim", 
+  name = "catppuccin",
+  priority = 1000,
+  config=function()
+    -- colorscheme setup
+    -- require("catppuccin").setup()
+    -- vim.cmd.colorscheme "catppuccin"
+  end
+}
