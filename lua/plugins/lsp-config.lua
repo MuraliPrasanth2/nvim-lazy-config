b9d097fb196d80c911ca097aef1f38600f5a4d91
Murali Prasanth <mprasanth18@gmail.com>
2024-02-22 21:51:32 +0530

    language server protocol working after installing the typescript-language-server in the path shown in the lsp.log

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -19,11 +19,11 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.tsserver.setup({})
---      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+      vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
---      vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-      vim.keymap.set({ 'n' }, '<space>ca', vim.lsp.buf.code_action, opts)
+      vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, opts)
     end
   }
 }
